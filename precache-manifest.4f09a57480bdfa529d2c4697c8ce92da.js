c36db513d83b83d41d2387e35b584d2496e34244
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2022-08-12 11:44:36 +0530

    Updates

---
+++ precache-manifest.4f09a57480bdfa529d2c4697c8ce92da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e028bb183b5cec58036b0d6cd754e2",
+    "url": "/To_Do/index.html"
+  },
+  {
+    "revision": "2db41e42c81d263727d7",
+    "url": "/To_Do/static/js/2.d73984dc.chunk.js"
+  },
+  {
+    "revision": "0c975fdab96e71c6fdce",
+    "url": "/To_Do/static/js/main.7f156656.chunk.js"
+  },
+  {
+    "revision": "546d8577d3568c339769",
+    "url": "/To_Do/static/js/runtime-main.c22197de.js"
+  }
+]);
